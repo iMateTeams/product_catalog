3b66c23b6c255c5c083b530a7029619836589182
NataliyaAn <otchet333@gmail.com>
2022-10-31 18:58:03 +0200

    router1

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 // import logo from './logo.svg';
@@ -11,7 +11,7 @@ import { Home } from './pages/Home';
 import { PhoneCardPage } from './pages/PhoneCardPage';
 import { TabletsPage } from './pages/TabletsPage';
 
-function App() {
+const App: React.FC = () => {
   return (
     <>
       <MainNav />
@@ -41,53 +41,6 @@ function App() {
       {/* <Footer /> */}
     </>
   );
-}
+};
 
 export default App;
-=======
-import React from 'react';
-import { Navigate, Route, Routes } from 'react-router-dom';
-// import logo from './logo.svg';
-// import { Counter } from './features/counter/Counter';
-import './App.css';
-import { MainNav } from './components/MainNav';
-import { AccessoriesPage } from './pages/AccessoriesPage';
-import { CatalogPage } from './pages/CatalogPage';
-import { Home } from './pages/Home';
-import { PhoneCardPage } from './pages/PhoneCardPage';
-import { TabletsPage } from './pages/TabletsPage';
-
-function App() {
-  return (
-    <>
-      <MainNav />
-      <div className="section">
-        <div className="container">
-          <Routes>
-            <Route
-              path="*"
-              element={<h1 className="title">Page not found</h1>}
-            />
-            <Route path="/" element={<Home />} />
-            <Route path="home" element={<Navigate to="/" replace />} />
-
-            <Route path="phones">
-              <Route index element={<CatalogPage/>} />
-              <Route path=":idPhone" element={<PhoneCardPage />} />
-            </Route>
-            <Route path="tablets">
-              <Route index element={<TabletsPage />} />
-            </Route>
-            <Route path="accessories">
-              <Route index element={<AccessoriesPage />} />
-            </Route>
-          </Routes>
-        </div>
-      </div>
-      {/* <Footer /> */}
-    </>
-  );
-}
-
-export default App;
->>>>>>> f1b2977 (router was added)
