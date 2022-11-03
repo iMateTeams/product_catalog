7a982af76ec43696c19bd99f2bc3fb83b43c14f0
Andrii Dunets <davbiznesman@gmail.com>
2022-11-03 10:19:41 +0200

    burger menu fixed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -67,7 +67,7 @@ const App: React.FC = () => {
     <>
       {/* <MainNav /> */}
       <Header onClick={() => setBurgerActive(!burgerActive)} clicked={burgerActive}/>
-      <BurgerMenu burgerActive={burgerActive}/>
+      <BurgerMenu onClick={() => setBurgerActive(!burgerActive)} clicked={burgerActive}/>
       <div className="section">
         <div className="container">
           <Routes>
