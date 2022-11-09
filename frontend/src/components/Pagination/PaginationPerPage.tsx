0255c2797fd02f41df04fe3e454efa090da864d6
NataliyaAn <otchet333@gmail.com>
2022-11-09 22:34:08 +0200

    sort and load were added

--- frontend/src/components/Pagination/PaginationPerPage.tsx
+++ frontend/src/components/Pagination/PaginationPerPage.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { SortBy } from '../../types/SortBy';
 import styles from './Pagination.module.scss';
 
 
@@ -7,6 +8,8 @@ type Props = {
   phonesPerPage: number
   setPhonesPerPage: (num: number) => void;
   setCurrentPage: (num: number) => void;
+  sortBy:SortBy | string;
+  setSortBy: (sort: SortBy | string) => void;
 }
 
 export const PaginationPerPage: React.FC<Props> = ({
@@ -14,29 +17,53 @@ export const PaginationPerPage: React.FC<Props> = ({
   phonesPerPage,
   setPhonesPerPage,
   setCurrentPage,
+  sortBy,
+  setSortBy,
 }) => {
   
 
   return(
-    <div>
-      <p className={styles.pagination_title}>
-        Items on page
-      </p>
-      <div className={styles.pagination_box}>
-        <select
-          className={styles.pagination_page}
-          value={phonesPerPage}
-          onChange={event => {
-            setPhonesPerPage(+event.target.value);
-            setCurrentPage(1);
-          }}
-        >
-          <option value="10" className={styles.pagination_page__option}>10</option>
-          <option value="16" className={styles.pagination_page__option}>16</option>
-          <option value="32" className={styles.pagination_page__option}>32</option>
-          <option value="50" className={styles.pagination_page__option}>50</option>
-          <option value={dataAmount} className={styles.pagination_page__option}>ALL</option>
-        </select>
+    <div className={styles.pagination} >
+      <div className={styles.pagination__section}>
+        <p className={styles.pagination__title}>
+          Sort By
+        </p>
+        <div className={styles.pagination__box}>
+          <select
+            className={styles.pagination__page}
+            value={sortBy}
+            onChange={event => {
+              setSortBy(event.target.value);
+              console.log(event.target.value);
+              setCurrentPage(1);
+            }}
+          >
+            <option value={SortBy.age} className={styles.pagination__page_sort}>{SortBy.age}</option>
+            <option value={SortBy.title} className={styles.pagination__page_sort}>{SortBy.title}</option>
+            <option value={SortBy.price} className={styles.pagination__page_sort}>{SortBy.price}</option>
+          </select>
+        </div>
+      </div>
+      <div className={styles.pagination__section}>
+        <p className={styles.pagination__title}>
+          Items on page
+        </p>
+        <div className={styles.pagination__box}>
+          <select
+            className={styles.pagination__page}
+            value={phonesPerPage}
+            onChange={event => {
+              setPhonesPerPage(+event.target.value);
+              setCurrentPage(1);
+            }}
+          >
+            <option value="10" className={styles.pagination__page_sort}>10</option>
+            <option value="16" className={styles.pagination__page_sort}>16</option>
+            <option value="32" className={styles.pagination__page_sort}>32</option>
+            <option value="50" className={styles.pagination__page_sort}>50</option>
+            <option value={dataAmount} className={styles.pagination__page_sort}>ALL</option>
+          </select>
+        </div>
       </div>
     </div>
   );
