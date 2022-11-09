dadac4ddd986ac28044e9bcdba37a652f881e95e
NataliyaAn <otchet333@gmail.com>
2022-11-09 22:50:19 +0200

    URL was changed on heroku

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,6 @@
 const express = require('express');
 const fs = require('fs');
-const path = require('path');
+// const path = require('path');
 const cors = require('cors');
 
 const PORT = process.env.PORT || 4002;
